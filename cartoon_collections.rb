4d5bf63384c8140bb7172a53f4849462514b7e19
Junko Tahara <junkotahara911@gmail.com>
2020-07-09 01:39:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,7 +22,7 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   
   planeteer_calls.each { |word|
-    puts word if valid_calls.any? { |call| call == word } 
+    if valid_calls.any? { |call| call == word } ? puts word : return nil
   }
   
   
